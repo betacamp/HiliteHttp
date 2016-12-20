0244abf3f075aa0866da7719bb29675405c9c292
Preston Pope <prestonwpope@gmail.com>
2016-12-19 21:09:44 -0800

    Initial Commit

---
+++ HiliteHttp/HiliteHttp.h
@@ -0,0 +1,19 @@
+//
+//  HiliteHttp.h
+//  HiliteHttp
+//
+//  Created by Preston Pope on 12/19/16.
+//  Copyright © 2016 betacamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HiliteHttp.
+FOUNDATION_EXPORT double HiliteHttpVersionNumber;
+
+//! Project version string for HiliteHttp.
+FOUNDATION_EXPORT const unsigned char HiliteHttpVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HiliteHttp/PublicHeader.h>
+
+
